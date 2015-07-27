8dd21e366e579372c137c32184358007971777d7
harryhare <harryhare@163.com>
2015-07-27 21:22:18 +0800

    level 16

---
+++ level16.py
@@ -0,0 +1,17 @@
+level_url='http://www.pythonchallenge.com/pc/return/uzi.html'
+import time
+import datetime
+begin=1582
+count=0
+for i in range(1586,1996,10):
+    if((i%4==0 and i%100!=0) or i%400==0):
+        target=datetime.date(i,1,1)
+        if(target.weekday()==3):
+            print(target)
+            count+=1
+            if(count==2):
+                break
+#http://worldhistoryproject.org/1756/1/27
+#https://en.wikipedia.org/wiki/Wolfgang_Amadeus_Mozart
+#mozart
+

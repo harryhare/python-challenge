a29efa15daf0617b99ed10cda6dafd7e740541a9
harryhare <harryhare@163.com>
2017-02-12 02:54:59 +0800

    add solution to level 22

---
+++ level22.py
@@ -0,0 +1,26 @@
+import zlib
+import bz2
+log=""
+file=open("./level20/package.pack","rb")
+reversed=False
+data=file.read()
+while True:
+    print(data[:2])
+    if(data[:2]=='x\x9c'):
+        data=zlib.decompress(data)
+        reversed=False
+        log+="z"
+    elif(data[:2]=="BZ"):
+        data=bz2.decompress(data)
+        reversed=False
+        log+='b'
+    elif(reversed==False):
+        data=data[::-1]
+        reversed=True
+        log+='r'
+    else:
+        break
+log=log.replace('r','\n')
+log=log.replace("z"," ")
+print log
+print log[::-1]
